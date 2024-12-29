9042a9dff35a80770155c38bc8a9cd797a266c6e
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-29 17:01:42 +0100

    chore: generated boilerplate for the nutrition calculation

---
+++ src/app/components/calorie-calculator/calorie-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calorie-calculator',
+  imports: [],
+  templateUrl: './calorie-calculator.component.html',
+  styleUrl: './calorie-calculator.component.css'
+})
+export class CalorieCalculatorComponent {
+
+}
---
+++ src/app/components/macro-calculator/macro-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-macro-calculator',
+  imports: [],
+  templateUrl: './macro-calculator.component.html',
+  styleUrl: './macro-calculator.component.css'
+})
+export class MacroCalculatorComponent {
+
+}
