8b0cc5f1fc7ade2cecf931ff6acad2a004caf712
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-29 16:53:30 +0100

    fix: fixxed wrong imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { ApiTestComponent } from './components/api-test/api-test.component';
 
 @NgModule({
-  declarations: [AppComponent, ApiTestComponent],
+  declarations: [],
   imports: [
     BrowserModule,
     HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
