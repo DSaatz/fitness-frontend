de3c10c731b2186877f193b48070e3f10e190e45
DSaatz <126864831+DSaatz@users.noreply.github.com>
2025-01-03 16:23:36 +0100

    feat: routing via navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  imports: [CommonModule, RouterModule]
 })
 export class NavbarComponent {
   isMobileMenuOpen = false;
